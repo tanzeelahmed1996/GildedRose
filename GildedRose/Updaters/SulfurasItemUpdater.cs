18452429ef4682248b070d2d9e5e6970b7318218
Tanzeel Ahmed <57502130+tanzeelahmed1996@users.noreply.github.com>
2026-04-18 11:52:40 +0100

    updated sulfuras

--- GildedRose/Updaters/SulfurasItemUpdater.cs
+++ GildedRose/Updaters/SulfurasItemUpdater.cs
@@ -5,9 +5,9 @@ public class SulfurasItemUpdater : IItemUpdater
     /// <summary>
     /// Sulfuras' quality does not change and it doesnt have to be sold.
     /// </summary>
-    /// <param name="item"></param>
+    /// <param name="item">Sulfuras Item</param>
     public void Update(Item item)
     {
-        
+        // Sulfuras does not change in quality or sell in, so we do nothing here.
     }
 }
